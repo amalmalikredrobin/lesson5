4e4dfde4f0060068faa6ffb53cf691fa6e1b0276
Amalmalik Abuzarov <amalmalikredrobin@gmail.com>
2021-11-13 17:27:57 +0300

    added

---
+++ index.py
@@ -0,0 +1,18 @@
+# name = input('Enter your name: ')
+# lastName = input('Enter your last name')
+
+# print('Hello, ' + name + ' ' + lastName)
+# print('Hello, {name} ,{lastName}')
+# print('Hello, {} {}'.format(name,lastName))
+
+# num1 = int(input('Enter 1st number: '))
+# num2 = int(input('Enter 2nd number: '))
+
+# print(f'{num1} + {num2} = {num1 + num2}')
+# print(f'{num1} - {num2} = {num1 - num2}')
+# print(f'{num1} / {num2} = {num1 / num2}')
+# print(f'{num1} * {num2} = {num1 * num2}')
+
+
+# print("I'm Amalmalik")
+
