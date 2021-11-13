0a762e19c3f250ea821359185793565a8e8b2fd1
Amalmalik Abuzarov <amalmalikredrobin@gmail.com>
2021-11-13 17:54:32 +0300

    Solved task for AND and OR operators

--- index.py
+++ index.py
@@ -16,3 +16,18 @@
 
 # print("I'm Amalmalik")
 
+# print(True and False)
+# print(True or False)
+# print(1 or False)
+# print(0 or False)
+# print('Amalmalik' or False)
+# print(2 or 55)
+# print(2 or 55 and False)
+# print(4 and 2 or False and 22)
+
+login = 'admin'
+password = 'admin'
+
+if login == 'admin' and password == 'admin': print('Welcome')
+else: print('Bye')
+
